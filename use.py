1dfb1b3ad9cff1366d3c4a9543c84629a57f47d8
Olivia <141013264+lilykprogramming@users.noreply.github.com>
2024-01-25 20:34:50 +0100

    Create use.py

---
+++ use.py
@@ -0,0 +1,25 @@
+from keras.models import load_model
+from keras.utils import load_img, img_to_array
+
+import numpy as np
+
+THOT = r'qrcods-014_acc-0.789614.h5'
+image_to_classification = r"" #your image to classification
+
+model = load_model(THOT)
+
+picture = load_img(image_to_classification, target_size=(224,224))
+
+picture = img_to_array(picture)
+
+picture = np.expand_dims(picture,axis=0)
+
+picture *=(1./255)
+
+results = model.predict(x=picture,steps=1,verbose=0)
+
+winner_index = np.argmax(results)
+
+print(results.max())
+
+print("Wydaje mi się, że to jest: ",winner_index)
